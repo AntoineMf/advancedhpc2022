9e9895a7fb10227b9e0084f9c8908d1978b21c0c
AntoineMf <itsezax@gmail.com>
2022-12-19 21:40:55 +0700

    cpu/gpu grayscale func

--- labwork3.py
+++ labwork3.py
@@ -1,6 +1,37 @@
 import matplotlib.pyplot as plt
 import numpy as np
+from numba import cuda
+import time
 
-image=plt.imread('img_forest.jpeg')
-gray_image = np.mean(image.reshape(-1, 3), axis=1).reshape(image.shape[0], image.shape[1])
-print(gray_image)
+
+def cpu_grayscale(image):
+    gray = 0.2989 * image[:,:,0] + 0.5870 * image[:,:,1] + 0.1140 * image[:,:,2]
+    return np.stack((gray, gray, gray), axis=2).astype(np.uint8)
+
+def gpu_grayscale_kernel(image, result):
+    height, width = image.shape
+    row = cuda.grid(1)
+    col = cuda.grid(2)
+    if row < height and col < width:
+        value = 0.2989 * image[row, col, 0] + 0.5870 * image[row, col, 1] + 0.1140 * image[row, col, 2]
+        result[row, col] = value
+
+def gpu_grayscale(image):
+    height, width, _ = image.shape
+    result = np.empty((height, width), dtype=np.float32)
+    threads_per_block = (16, 16)
+    blocks = (height // threads_per_block[0] + 1, width // threads_per_block[1] + 1)
+    gpu_grayscale_kernel[blocks, threads_per_block](image, result)
+    return result
+
+
+if __name__=='__main__':
+    image=plt.imread('img_forest.jpeg')
+    start_time_cpu = time.time()
+    cpu_output=cpu_grayscale(image)
+    print(cpu_output)
+    print("--- %s seconds ---" % (time.time() - start_time_cpu))
+    start_time_gpu = time.time()
+    gpu_output=gpu_grayscale(image)
+    print(gpu_output)
+    print("--- %s seconds ---" % (time.time() - start_time_gpu))
