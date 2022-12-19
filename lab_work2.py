1ac3c3a98ce10012dc6f77950c195b66a01c0290
AntoineMf <itsezax@gmail.com>
2022-12-19 18:16:16 +0700

    syntax correction

--- lab_work2.py
+++ lab_work2.py
@@ -2,6 +2,6 @@ from numba import cuda
 print(f"Available GPU : {cuda.is_available()}")
 print(f'list of GPU : {cuda.detect()}')
 dev=cuda.select_device(0)
-print('Device name: ', dev.name)
+print(f'Device name: {dev.name}')
 print(f'number of multiprocessor: {dev.MULTIPROCESSOR_COUNT}')
 print(f'Total memory size: {cuda.current_context().get_memory_info().total}')
