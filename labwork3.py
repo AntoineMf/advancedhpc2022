42590d7425b861a811b91b46147c0959b7874be5
AntoineMf <itsezax@gmail.com>
2022-12-20 19:24:22 +0700

    GPU updating

--- labwork3.py
+++ labwork3.py
@@ -5,33 +5,39 @@ import time
 
 
 def cpu_grayscale(image):
-    gray = 0.2989 * image[:,:,0] + 0.5870 * image[:,:,1] + 0.1140 * image[:,:,2]
-    return np.stack((gray, gray, gray), axis=2).astype(np.uint8)
+    # Check if image is already grayscale
+    if len(image.shape) == 2:
+        return image
 
+    # Convert image to grayscale using luminosity formula
+    return 0.21 * image[:,:,0] + 0.72 * image[:,:,1] + 0.07 * image[:,:,2]
+
+@cuda.jit
 def gpu_grayscale_kernel(image, result):
-    height, width = image.shape
-    row = cuda.grid(1)
-    col = cuda.grid(2)
-    if row < height and col < width:
-        value = 0.2989 * image[row, col, 0] + 0.5870 * image[row, col, 1] + 0.1140 * image[row, col, 2]
-        result[row, col] = value
+    height = image.shape[0]
+    width = image.shape[1]
+    i, j = cuda.grid(2)
+    if i >= height or j >= width:
+        return
+
+    result[i, j] = (image[i, j, 0] * 0.3 + image[i, j, 1] * 0.59 + image[i, j, 2] * 0.11)
 
 def gpu_grayscale(image):
     height, width, _ = image.shape
     result = np.empty((height, width), dtype=np.float32)
     threads_per_block = (16, 16)
     blocks = (height // threads_per_block[0] + 1, width // threads_per_block[1] + 1)
-    gpu_grayscale_kernel[blocks, threads_per_block](image, result)
+    start_time_gpu = time.time()
+    gpu_grayscale_kernel[blocks, threads_per_block](image.copy(), result)
+    print("--- GPU %s seconds ---" % (time.time() - start_time_gpu))
     return result
 
 
-if __name__=='__main__':
-    image=plt.imread('img_forest.jpeg')
-    start_time_cpu = time.time()
-    cpu_output=cpu_grayscale(image)
-    print(cpu_output)
-    print("--- %s seconds ---" % (time.time() - start_time_cpu))
-    start_time_gpu = time.time()
-    gpu_output=gpu_grayscale(image)
-    print(gpu_output)
-    print("--- %s seconds ---" % (time.time() - start_time_gpu))
+image=plt.imread('big_image.jpg')
+start_time_cpu = time.time()
+cpu_output=cpu_grayscale(image)
+print(cpu_output)
+print("--- CPU %s seconds ---" % (time.time() - start_time_cpu))
+
+gpu_output=gpu_grayscale(image)
+print(gpu_output)
